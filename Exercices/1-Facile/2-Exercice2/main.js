086375947b3e9c35e120b9aa85ba83e17de44fad
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-01 18:34:55 +0100

    Maitrisez-le-JavaScript-en-30-exercices-pratiques-2-Exercice2

---
+++ Exercices/1-Facile/2-Exercice2/main.js
@@ -0,0 +1,11 @@
+var nom = "Toto";
+var age = 30;
+var estUnHomme = true;
+
+console.log("Nom : %s ", nom);
+console.log("Age : %d ", age);
+if(!estUnHomme) {
+    console.log("Sexe : Femme");
+} else {
+    console.log("Sexe : Homme");
+}
