9b6aa122883945b49f19ec85ca636a4932966878
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-07 15:04:24 +0100

    Maitrisez-le-JavaScript-en-30-exercices-pratiques-Difficile-03-Exercice03-GestionDArme-01-LeMain

---
+++ Exercices/3-Difficile/3-Exercice3/armeManager.js
@@ -0,0 +1,13 @@
+var armeManager = {
+    afficherArmes : function() {
+        console.log("Affichage des armes");
+    },
+    ajouterArme : function() {
+        console.log("Ajout d'arme");
+    },
+    supprimer : function() {
+        console.log("Suppression d'arme");
+    },
+
+}
+module.exports = armeManager;
---
+++ Exercices/3-Difficile/3-Exercice3/main.js
@@ -0,0 +1,35 @@
+var readline = require('readline-sync');
+
+var armeManager = require('./armeManager.js');
+
+var choixMenu = -1;
+
+while(choixMenu !== 0) {
+    menu();
+    choixMenu = parseInt(readline.question("Quel est votre choix ? "));
+    switch(choixMenu) {
+        case 1 : // Affichages des armes
+            armeManager.afficherArmes();
+        break;
+        case 2 : // Ajouter une arme
+            armeManager.ajouterArme();
+        break;
+        case 3 : // Supprimer une arme
+            armeManager.supprimer();
+        break;
+        case 0 : console.log("A +");
+        break;
+        default : console.log("Cas inconnu");
+        break;
+    }
+
+}
+
+function menu() {
+    var txt = "";
+    txt += "1/ Afficher les armes \n";
+    txt += "2/ Ajouter une arme \n";
+    txt += "3/ Supprimer une arme \n";
+    txt += "0/ Quitter \n";
+    console.log(txt);
+}
