3ca3ba58f758254fe33531798910a2d87b9db127
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-07 15:20:40 +0100

    Maitrisez-le-JavaScript-en-30-exercices-pratiques-Difficile-03-Exercice03-GestionDArme-02-CreationEtAffichageDuFichierJSON

--- Exercices/3-Difficile/3-Exercice3/armeManager.js
+++ Exercices/3-Difficile/3-Exercice3/armeManager.js
@@ -1,6 +1,16 @@
+var types = require('./json/types.json');
+var armes = require('./json/armes.json');
+
 var armeManager = {
     afficherArmes : function() {
-        console.log("Affichage des armes");
+        console.log(armes, types);
+        for(var arme in armes) {
+            var idArme = arme.substr(-1,1);
+            console.log(idArme + " : " + this.afficherArme(armes[arme]));
+        }
+    },
+    afficherArme : function(arme) {
+        return "Nom : " + arme.nom +" - Type : " + types[arme.type];
     },
     ajouterArme : function() {
         console.log("Ajout d'arme");
